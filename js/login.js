b2cf0e2ef90786c41bf8f47dd5bdb5a1ba60babf
sumin kim <marianne.gg53@gmail.com>
2023-06-11 08:12:54 +0000

    로그인 - 쿠키까지 보완

--- js/login.js
+++ js/login.js
@@ -1,41 +1,60 @@
-function login()
-{
+function login() {
     let form = document.querySelector("#form_main");
     let id = document.querySelector("#floatingInput");
-    let password = document.querySelector("#floatingPassword");
-    
-    form.action = "../login/index_login.html";
-	form.method = "get";
-	
-	if (id.value.length === 0)
-	{
-		alert('아이디를 입력하세요');
-	}
-	
-	else if (password.value.length === 0)
-	{
-		alert('패스워드를 입력하세요');
-	}
-	
-	else
-	{
-		form.submit();
-	}
+    let password = document.querySelector("#floatingPassword")
+    let check = document.querySelector("#idSaveCheck");
+
+    if (check.checked == true) {
+        alert("쿠키를 저장합니다.");
+        setCookie("id", id.value, 1);
+        alert("쿠키 값 :" + id.value);
+    } else {
+        setCookie("id", id.value, 0);
+    }
+
+
+    if (id.value.length === 0) {
+        alert('이메일을 입력하세요');
+    } else if (password.value.length === 0) {
+        alert('패스워드를 입력하세요');
+    } else {
+        if (!email_check(id.value)) {
+            alert("이메일 형식에 맞게 입력해주세요.");
+        } else {
+            if (!password_check(password.value)) {
+                alert("패스워드 형식에 맞게 입력해주세요.");
+            } else {
+                form.action = "../login/index_login.html";
+                form.method = "get";
+                form.submit();
+            }
+        }
+    }
 }
 
-function get_id(){
-	var getParameters = function(paramName){
-		var retyrnValue;
-		var url = location.href;
-		var parameters = (url.slice(url.indexOf("?") + 1, url.length)).split('&');
-		for (var i = 0; i<parameters.length; i ++){
-			var varName = parameters[i].split('=')[0];
-		
-			if (varName.toUpperCase() == paramName.toUpperCase()){
-				returnValue = parameters[i].split('=')[1];
-				return decodeURIComponent(returnValue);
-			}
-		}
-	}
-	alert(getParameters('id') + '님 반갑습니다.');
+function email_check(email) {
+    let idregex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    return idregex.test(email);
+}
+
+function password_check(password) {
+    let passregex = /^[a-zA-Z][A-Za-z\d@$!%*?&]{8,}$/i;
+    return passregex.test(password);
+}
+
+function get_id() {
+    var getParameters = function(paramName) {
+        var retyrnValue;
+        var url = location.href;
+        var parameters = (url.slice(url.indexOf("?") + 1, url.length)).split('&');
+        for (var i = 0; i < parameters.length; i++) {
+            var varName = parameters[i].split('=')[0];
+
+            if (varName.toUpperCase() == paramName.toUpperCase()) {
+                returnValue = parameters[i].split('=')[1];
+                return decodeURIComponent(returnValue);
+            }
+        }
+    }
+    alert(getParameters('id') + '님 반갑습니다.');
 }
--- js/search.js
+++ js/search.js
@@ -1,8 +1,43 @@
 document.getElementById("search_btn").addEventListener('click', search_message);
 
-function search_message(){
-   alert("검색을 수행합니다!");
-   let search_str = document.querySelector("#search_txt"); // 변수에 저장
-   document.getElementById("search_message").innerHTML = search_str.value; // 태그에 값 추가
-   console.log(search_str.value); // 콘솔에 출력
+var Not_allowed = ["냠냠","쩝쩝","꿀꺽"];
+var blocked = false;
+message = [];
+
+function search_message()
+{
+    let search_str = document.querySelector("#search_txt");
+    
+	if (search_str.value.length === 0)
+	{
+       alert("검색어가 비었습니다. 입력해주세요"); 
+    }
+	
+	else 
+	{
+    	for (var n = 0; n < Not_allowed.length; n++)
+		{
+			if (search_str.value.includes(Not_allowed[n]))
+			{
+				alert("유효하지 않은 검색어입니다.");
+				blocked = true;
+				break;
+			}
+		}
+		
+		if (blocked == false)
+		{
+			alert("검색을 수행합니다!");
+   			console.log(search_str.value); 
+			document.querySelector("#form_main").submit();
+			
+			message.push(search_str.value);
+			
+			if (message.length > 5){
+				message.shift();
+			}
+			document.querySelector("#search_message").innerHTML = message;
+		}
+	}
+	blocked = false;
 }
